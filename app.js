c9bcaf10d435c21f3f83e05a8f800e51dc115298
Cbodtorf <cbodtorf@gmail.com>
2016-10-25 17:51:36 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', function(){
+  app.init();
+})
+
+var app = {
+  init: function() {
+    this.styles()
+    this.events()
+  },
+  styles: function() {
+
+  },
+  events: function() {
+    var sandwichLayersRange = document.querySelector('.sandwich-layers')
+    var imgLayerWrappers = document.querySelectorAll('.img-wrapper:not(:first-of-type)')
+    var imgLayers = document.querySelectorAll('.img-wrapper img')
+    var rotateLayersRange = document.querySelector('.rotate-layers')
+
+
+    sandwichLayersRange.addEventListener('input', function(){
+      console.log(sandwichLayersRange.value);
+
+      var tVal = sandwichLayersRange.value -130;
+      imgLayerWrappers.forEach(function(e){
+        e.style.marginLeft = tVal + 'px'
+      })
+
+      var rVal = sandwichLayersRange.value -30;
+      imgLayers.forEach(function(e){
+        console.log("tranform", e.style.transform);
+        e.style.transform = "rotate3d(-0.2,1,-0.1," + rVal + "deg)"
+      })
+    })
+
+    // rotateLayersRange.addEventListener('input', function(){
+    //   console.log(rotateLayersRange.value);
+    //
+    //   imgLayers.forEach(function(e){
+    //     console.log("tranform", e.style.transform);
+    //     e.style.transform = "rotate3d(0,1,0," + rotateLayersRange.value + "deg)"
+    //   })
+    // })
+
+  }
+
+}
